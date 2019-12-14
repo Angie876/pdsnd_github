cfbffc239e2fbe6ca8d8543d24826234ca835ce7
Adipster Ltd <adipster@localhost.localdomain>
2019-12-14 11:22:35 +0100

    feat: Edited the docstring of the filter

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks the user to specify a city, month, and day to analyze and filter.
 
     Returns:
         (str) city - name of the city to analyze
